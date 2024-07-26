00086973f670ee4f36adb65245d96cee5896e6fd
MPFlow <marcosgarcia2485@gmail.com>
2024-07-25 23:58:03 -0400

    created code that executes all test correctly

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+
+  let text = "";
+
+  if (times < 0) return "ERROR";
+
+  for (i = 0; i < times; i++) {
+    text += string;
+  }
+  
+  return text;
 
 };
 
