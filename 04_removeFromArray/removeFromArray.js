e5a8d9db2f147436c66a3df304e536b772b3fc5e
MPFlow <marcosgarcia2485@gmail.com>
2024-07-26 17:42:02 -0400

    checking to see if first argument is an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
 const removeFromArray = function() {
+
+
+ // check if the first part of the function argument is an array
+ if (Array.isArray(arguments[0]) === true)
+ {
+    array = arguments[0];    
+           
+ }
+
+let word = "";
+for (i = 0; i < array.length; i++)
+{
+    word += array[i];
+}
+
+ return word;
+
+
 };
+
+removeFromArray([1,2,3,4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
