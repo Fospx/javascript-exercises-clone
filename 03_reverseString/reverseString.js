daf1348969357a5d219640dac06c1759785911d7
MPFlow <marcosgarcia2485@gmail.com>
2024-07-26 16:39:36 -0400

    added for loop that reverses strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+let word = "";
+let length = string.length;
+
+    for (i = length - 1; i >= 0; i--)
+    {
+        word += string[i];
+    }
+return word;
 
 };
 
