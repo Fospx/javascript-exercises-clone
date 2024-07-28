49f2bbe638032ce437e75dc139e05c9f9c58ca87
MPFlow <marcosgarcia2485@gmail.com>
2024-07-27 21:33:25 -0400

    added code that accepts all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(minNum, maxNum) {
+    
+    if (!Number.isInteger(minNum) || !Number.isInteger(maxNum))
+    {
+        return "ERROR";
+    }
+    if (minNum < 0 || maxNum < 0)
+    {
+        return "ERROR";
+    }
+    if (minNum > maxNum)
+    {
+        let temp = minNum;
+        minNum = maxNum;
+        maxNum = temp;
+    }
+
+    let sum = 0;
+    for (let i = minNum; i <= maxNum; i++)
+    {
+        sum += i;
+    }
+    return sum;
+
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
