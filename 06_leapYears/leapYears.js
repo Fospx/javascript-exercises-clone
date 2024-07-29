3a0e0d70a5aa8923969d6c4774e2fd3621740b54
MPFlow <marcosgarcia2485@gmail.com>
2024-07-28 21:58:40 -0400

    added psuedocode to help me figure out the structure of my code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    let answer = 0;
+    let check_if_integer;
+    answer = year / 4
+    check_if_integer = Number.isInteger(answer);
+
+    if (check_if_integer === true)
+    {
+        return true;
+    }
+    else return false;
 
 };
+
+leapYears(2000);
 
 // Do not edit below this line
 module.exports = leapYears;
