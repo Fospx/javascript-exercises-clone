a536c6cac974dd13a328b298c5df1bfe39635a06
MPFlow <marcosgarcia2485@gmail.com>
2024-07-29 20:03:19 -0400

    created code that accepts all the test provided

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,37 +1,38 @@
 const leapYears = function(year) {
 
+    // Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible 
+    // by 400 (like 1600 and 2000, which were in fact leap years). //
 
-    // create a variable to hold the value of the year
-    // make the calculations, //
-    // see if it is divisable by 4 and gives us an integer and/or check to see if it is divisble by 400 and gives us an integer
-    // if it does give an integer... we are not done yet because....
-    // check to see if dividing by 100 gives us an integer
-    // if it does, then return false because the year divided by 100 is not a leap year
-    // else if it does not return an integer then it is a leap year and divisble by 4 or by 400.
 
-let leapYears; 
+    // var to hold leapYear true/false value
+    // if year divided by 4 gives a remainder of 0 then it is a leap year
+    // also check if the year gives a remainder of 0 when divided by 100
+    // if it gives a remainder of 0 then it is not a leap year
 
-if ((year % 4) === 0)
-{
-    if ((year % 4) === 0 && (year % 100) === 0)
+
+    let leapYear;
+
+    // checks to see if 100 is divis and also checks to see if it is divis by 400 to make sure it is a leapyear
+    if ((year % 100) === 0 && (year % 400) === 0)
     {
-        leapYears = false
-        return leapYears
+        leapYear = true;
+        return leapYear;
     }
-    leapYears = true;
-    return leapYears;
-}
 
-if ((year % 100) === 0) {
-    if ((year % 400) === 0)
+    // first if statement: divides by 4 and checks if the remainder is 0 and returns true 
+    // second if statement: divides by 100 and checks if remainder is 0 and returns false 
+    if ((year % 4) === 0)
     {
-        leapYears = true;
-        return leapYears;
+        if ((year % 100 === 0))
+        {
+            leapYear = false;
+            return leapYear;
+        }
+        leapYear = true;
+        return leapYear;
     }
-    leapYears = false;
-    return leapYears
-}
-
+    // returns false if everything does not apply
+    return false;
 }
 leapYears(1997);
 
