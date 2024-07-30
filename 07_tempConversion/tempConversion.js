a666728d825d5e0ce79742f8df395853e29b092a
MPFlow <marcosgarcia2485@gmail.com>
2024-07-29 22:42:23 -0400

    added code to convert celsius to fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,26 +1,30 @@
 // create a var to hold the answer
 const convertToCelsius = function(fahrenheit) {
 
+// var holder for answer
+let celsius_answer = 0; 
+
 // F = (x-32) * 5/9
-
-let fahrenheit_answer = 0; 
-
-fahrenheit_answer = Math.round((fahrenheit - 32) * 5/9) 
-
-return fahrenheit_answer;
+// formula calculation to convert fahrenheit to celsius
+celsius_answer = Math.round((fahrenheit - 32) * 5/9) 
+return celsius_answer;
 
 };
 
 const convertToFahrenheit = function(celsius) {
 
 // C = (x * 9/5 + 32)
+let fahrenheit_answer = 0;
 
+fahrenheit_answer = Math.round((celsius * 9/5 + 32))
+return fahrenheit_answer;
 
 
 };
 
 
 convertToCelsius(32);
+convertToFahrenheit(0)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
