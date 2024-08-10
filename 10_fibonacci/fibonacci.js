171502180fade32f9863134b583fbda0a33f1e9a
MPFlow <marcosgarcia2485@gmail.com>
2024-08-10 11:50:38 -0400

    negative numbers dont work, strings work like a number, made comments explaining

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -12,6 +12,15 @@ else if (position == 1 || position == 2)
 {
     return 1;
 }
+if (position < 0) return "OOPS";
+
+// checks if the argument is a string 
+if (typeof position !== "number")
+{
+    // if it passes as not a number then it converts it to a string
+    position = parseInt(position);
+}
+
 
 
 // if we got here then start with the 3rd position
@@ -20,7 +29,6 @@ const fib = [0, 1];
 for (i = 2; i <= position; i++)
 {
     fib[i] = fib[i - 1] + fib[i - 2];
-
 }
 
 return fib[position];
