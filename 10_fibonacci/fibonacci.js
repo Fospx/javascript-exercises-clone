523e4f7b29f62c77979ac799aef11e6c64e5c04f
MPFlow <marcosgarcia2485@gmail.com>
2024-08-10 11:37:27 -0400

    adds calculation onto array and returns that position in the array for the solution using the fib formula

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
 
-};
+// calculate the numbers 
+// use the position as the length of how far we want to calculate 
+// calculate until we hit the position
+// return the position
+
+if (position == 0){
+    return 0;
+}
+else if (position == 1 || position == 2)
+{
+    return 1;
+}
+
+
+// if we got here then start with the 3rd position
+
+const fib = [0, 1];
+for (i = 2; i <= position; i++)
+{
+    fib[i] = fib[i - 1] + fib[i - 2];
+
+}
+
+return fib[position];
+
+}
+fibonacci(6)
 
 // Do not edit below this line
 module.exports = fibonacci;
