d1958b1a552c08ea7d17bf46eab9bb6542b34e54
MPFlow <marcosgarcia2485@gmail.com>
2024-08-10 18:55:59 -0400

    yes

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -4,18 +4,5 @@ return books.map((book) => book.title)
 
 };
 
-const books = [
-    {
-      title: 'Book',
-      author: 'Name'
-    },
-    {
-      title: 'Book2',
-      author: 'Name2'
-    }
-  ]
-
-  getTheTitles(books)
-
 // Do not edit below this line
 module.exports = getTheTitles;
